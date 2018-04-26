8aadba09853ca835c0100bd033d26292806c3298
liushuangbo <liushuangbo@sinosoft.com.cn>
2018-04-26 16:55:48 +0800

    设计模式-代理模式

--- Proxy/src/main/java/com/lius/proxy/Medium.java
+++ Proxy/src/main/java/com/lius/proxy/Medium.java
@@ -1,8 +1,13 @@
 package com.lius.proxy;
 
+import java.util.Random;
+
 /**
  * 中介
  * Proxy类
+ * 代理模式与装饰模式类似
+ * 装饰模式强调扩展
+ * 代理模式强调限制，可以决定被代理对象行为的执行
  * Created by liushuangbo on 2018/4/26.
  */
 public class Medium implements Pay {
@@ -16,7 +21,12 @@ public class Medium implements Pay {
 
     public void rent() {
         if (tenant != null) {
-            tenant.rent();
+            Random random = new Random();
+            if (random.nextBoolean()) {
+                tenant.rent();
+            } else {
+                System.out.println("还没到交租时间~");
+            }
         }
     }
 

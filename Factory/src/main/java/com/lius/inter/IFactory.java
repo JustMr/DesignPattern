d62f44b55709fccb336ba28fd3643bf336f2fc6a
liushuangbo <liushuangbo@sinosoft.com.cn>
2018-04-28 13:54:40 +0800

    设计模式 - 装饰模式

--- Factory/src/main/java/com/lius/inter/IFactory.java
+++
@@ -1,9 +0,0 @@
-package com.lius.inter;
-
-/**
- * Creator接口
- * Created by liushuangbo on 2018/4/26.
- */
-public interface IFactory {
-    public IOperator createFactory();
-}
--- Proxy/src/main/java/com/lius/proxy/Pay.java
+++
@@ -1,9 +0,0 @@
-package com.lius.proxy;
-
-/**
- * Subject类
- * Created by liushuangbo on 2018/4/26.
- */
-public interface Pay {
-    public void rent();
-}
